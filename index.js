ee234bb6681abe8d269dbd72f8f339e8a9ed8c47
Senpai__ <82378971+Senpai021@users.noreply.github.com>
2022-05-05 10:20:45 +0200

    index.js 1.1
    Added boring button function!!!

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 function myFunction() {
 	var x = document.getElementById("myLinks");
 	if (x.style.display === "block") {
-	  x.style.display = "none";
+		x.style.display = "none";
 	} else {
-	  x.style.display = "block";
+		x.style.display = "block";
 	}
-  }
+}
+
+const incrementButton = document.getElementById("bored")
+
+incrementButton.innerHTML = parseInt(localStorage.getItem("points") || 0)
+
+function increment() {
+	let newValue = ++incrementButton.innerHTML
+
+	incrementButton.innerHTML = newValue
+	localStorage.setItem("points", newValue)
+}
+
+incrementButton.addEventListener("click", () => {
+	increment()
+})
