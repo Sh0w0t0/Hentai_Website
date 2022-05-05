e6241295f87d0e7ad6c62f4b777ea5e52613b01f
Michael Steinmötzger <m.steinmoetzger@gmail.com>
2022-05-05 09:26:01 +0200

    added swagger documentation to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder } from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +11,11 @@ async function bootstrap() {
     .addBearerAuth()
     .setVersion("0.1-Dev")
     .build();
+
+  const document = SwaggerModule.createDocument(app, options)
+
+  // Setup swagger documentation on path docs, here because we want a documentation, but can be removed in production
+  SwaggerModule.setup("docs", app, document)
 
   await app.listen(3000);
 }
